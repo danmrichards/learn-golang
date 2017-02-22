92350e0a22096d4bdceaf3e86fbb3965efed0d70
Dan Richards <dan.m.richards@gmail.com>
2017-02-22 23:00:13 +0000

    Little go book - Variables

---
+++ little-go-book/variables/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+    // Short hand for this (power := 9000) will infer the type.
+    var power int
+    power = 9000
+
+    fmt.Printf("It's over %d\n", power)
+}
