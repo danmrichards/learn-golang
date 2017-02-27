8afe1277f46f3502fef8bcf904ec718516044fbb
Dan Richards <dan.m.richards@gmail.com>
2017-02-27 21:42:43 +0000

    Little go book - Maps.

---
+++ little-go-book/21-maps/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "fmt"
+)
+
+// Maps in Go are what other languages call hashtables or dictionaries. They
+// work as you expect: you define a key and value, and can get, set and
+// delete values from it.
+func main() {
+    lookup := make(map[string]int)
+
+    // Maps grow dynamically. However, we can supply a second argument to make
+    // to set an initial size. If you have some idea of how many keys your map
+    // will have, defining an initial size can help with performance.
+    // lookup := make(map[string]int, 100)
+
+    // Assign a value.
+    lookup["goku"] = 9001
+
+    // A shorthand way to create and assign a slice is similar to arrays.
+    // lookup := map[string]int{
+    //    "goku": 9001,
+    //    "gohan": 2044,
+    // }
+
+    // Prints 0, false
+    // 0 is the default value for an integer
+    power, exists := lookup["vegeta"]
+    fmt.Println(power, exists)
+
+    // Returns 1
+    total := len(lookup)
+    fmt.Printf("The map has a length of %d\n", total)
+
+    // Has no return, can be called on a non-existing key
+    delete(lookup, "goku")
+}
---
+++ little-go-book/22-maps-structures/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+    "fmt"
+)
+
+// Saiyan structure.
+// Fields can be of any type – including maps of other structures.
+type Saiyan struct {
+    Name string
+    Power int
+    Friends map[string]*Saiyan
+}
+
+func main() {
+    goku := &Saiyan{
+        Name: "Goku",
+        Power: 9001,
+        Friends: make(map[string]*Saiyan),
+    }
+
+    goku.Friends["krillin"] = &Saiyan{
+        Name: "Krillin",
+        Power: 1000,
+        Friends: nil,
+    }
+
+    goku.Friends["dan"] = &Saiyan{
+        Name: "Dan",
+        Power: 999999,
+        Friends: nil,
+    }
+
+    fmt.Printf("%s has a power level of %d\n", goku.Name, goku.Power)
+
+    for _, friend := range goku.Friends {
+        fmt.Printf("His friend %s has a power level of %d\n", friend.Name, friend.Power)
+    }
+}
