0752d30fdda68814d4dab53c94b3d9d6a5eba8f3
Dan Richards <dan.m.richards@gmail.com>
2017-02-28 15:47:52 +0000

    Little go book - Packages.

---
+++ little-go-book/23-shopping/db/db.go
@@ -0,0 +1,13 @@
+package db
+
+// Item - A shopping item.
+type Item struct {
+    Price float64
+}
+
+// LoadItem - Loads an item and returns it.
+func LoadItem(id int) *Item {
+    return &Item{
+        Price: 9.001,
+    }
+}
